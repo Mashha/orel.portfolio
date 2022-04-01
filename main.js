f0f9bb6fbbb01e1c76263f43a2699adf91e88a1a
Masa Orel <orel.masha89@gmail.com>
2022-04-01 13:38:00 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const slide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".links");
+    const navLinks = document.querySelectorAll(".links li");
+  
+  
+    //toggle nav
+    burger.addEventListener("click", ()=> {
+      
+      nav.classList.toggle("active");
+  
+      navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index/7}s`;
+        }
+          
+      });
+  
+      burger.classList.toggle("toggle");
+  
+    });
+  
+  }
+  
+  slide();
